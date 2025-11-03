4629e4c12530b2573a3a8ab0edb2543155cd270a
Rafael García Álvarez <vattenbit@gmail.com>
2025-11-03 20:26:54 +0100

    Add validBraces solution

--- codewars_swift/5277c8a221e209d3f6000b56/validBraces.swift
+++ codewars_swift/5277c8a221e209d3f6000b56/validBraces.swift
@@ -1,35 +1,43 @@
-func validParentheses(_ str: String) -> Bool {
-    var count: Int = 0
+func validBraces(_ str: String) -> Bool {
+    var stack: [Character] = []
+    let matching_bracket: [Character: Character] = [")": "(", "]": "[", "}": "{"]
     for char in str {
-        if char == "(" {
-            count += 1
-        }else if char == ")" {
-            count -= 1
-            if count < 0 {
+        switch char {
+        case "(", "[", "{":
+            stack.append(char)
+        case ")", "]", "}":
+            let last = stack.popLast()
+            if last != matching_bracket[char] {
                 return false
             }
+        default: continue
         }
     }
-    if count != 0 {
-        return false
-    }
-    return true
+    return stack.isEmpty
 }
 
-@main
-struct Tests {
-    static func main() {
-        doTest(true, "()", using: validParentheses)
-        doTest(true, "((()))", using: validParentheses)
-        doTest(true, "()()()", using: validParentheses)
-        doTest(true, "(()())()", using: validParentheses)
-        doTest(true, "()(())((()))(())()", using: validParentheses)
-        doTest(false, ")(", using: validParentheses)
-        doTest(false, "()()(", using: validParentheses)
-        doTest(false, "((())", using: validParentheses)
-        doTest(false, "())(()", using: validParentheses)
-        doTest(false, ")()", using: validParentheses)
-        doTest(false, ")", using: validParentheses)
-        doTest(true, "", using: validParentheses)
+func ValidBracesTests() {
+    let array_tests = [
+        // testNonNested
+        (true, "()"),
+        (true, "[]"),
+        (true, "{}"),
+        (true, "(){}[]"),
+        // testNested
+        (true, "([{}])"),
+        (false, "(}"),
+        (false, "[(])"),
+        (true, "({})[({})]"),
+        (false, "(})"),
+        (true, "(({{[[]]}}))"),
+        (true, "{}({})[]"),
+        (false, ")(}{]["),
+        (false, "())({}}{()][]["),
+        (false, "(((({{"),
+        (false, "}}]]))}])")
+    ]
+    
+    for test in array_tests {
+        doTest(test.0, test.1, using: validBraces)
     }
 }
